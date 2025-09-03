a686e5345a4d433d1d2be829081832f79a33081f
Carbon108 <9625791+carbon108@users.noreply.github.com>
2025-09-03 19:22:21 +0300

    ObjectInfo

--- tests/test_abc.py
+++ tests/test_abc.py
@@ -28,88 +28,6 @@ from c108.tools import print_method, print_title
 
 # Classes --------------------------------------------------------------------------------------------------------------
 
-class ObjClass:
-    b: int = 1
-    a: int = 0
-    _private: int = 0
-    __dunder: int = 0
-
-    def __init__(self):
-        self.c = "c"
-        self.d = "d"
-        self.none = None
-
-    def a_method(self):
-        return 0
-
-
-@dataclass
-class DatClass:
-    b: int = 1  # field is instance attr
-    a: int = 0  # field is instance attr
-
-    def a_method(self):
-        return 0
-
-
-# Non-frozen Dataclass
-@dataclass
-class DatDynamic:
-    aa: int = 12  # field is instance attr
-    bb: int = 24  # field is instance attr
-
-
-# Frozen Dataclass
-@dataclass(frozen=True)
-class DatFrozen:
-    p: int = 0
-
-
-class ObjAsDict:
-    a = 0  # class attr
-    to_dict = {"a": "zero"}
-
-
-class ObjAsDictCallable:
-    aa = 0  # class attr
-
-    def to_dict(self):
-        return {"aa": "zero"}
-
-
-@dataclass
-class DataClass:
-    a = 0  # !!! <-- without type this is a class attr but NOT a dataclass field
-    b: int = 1
-    c: int = field(default=2)
-    d: DatDynamic = field(default_factory=DatDynamic)
-    o: type = ObjAsDict
-
-
-@dataclass
-class DatClassDeep:
-    a: int = 0
-    d: DatDynamic = field(default_factory=DatDynamic)
-    f: DatFrozen = DatFrozen(p=1)
-    dict: dict = field(default_factory=dict)
-    list: list = field(default_factory=list)
-    _private: int = 0
-    __dunder: int = 22
-    ___tripler: int = 333
-
-    @property
-    def property_good(self):
-        return self.a + 1
-
-    @property
-    def property_except(self):
-        raise ValueError("Non-calculatable property")
-
-    def dat_method(self):
-        return 0
-
-
-# python
 class FakeImage:
     # emulate PIL.Image.Image's API via properties/methods on the class
     def __init__(self, size=(10, 20), mode="RGB", fmt="PNG"):
@@ -133,54 +51,6 @@ class FakeImage:
     def resize(self, size): return FakeImage(size=size, mode=self.mode, fmt=self.format)
 
     def crop(self, box): return self
-
-
-class Testable:
-    instances: list
-    classes: list
-
-    def __init__(self):
-        # Primitives
-        primitives = [int(1), float(2), bool(True), complex(1 + 2j), "QWERTY"]
-        self.primitives = primitives
-
-        # Instances
-        items = (
-            [1], (1, 2),
-            {'a': 1, 'b': 2, 'c': 2}, {1, 2, 3}, frozenset({1, 2, 3}),
-            range(0, 14)
-        )
-        bytes_ = bytes("QWERTY", "utf-8")
-        image_ = FakeImage()
-        instance_obj = ObjClass()
-        instanse_datdeep = DatClassDeep(dict={1: "one", 2: "two"}, list=[1, 2, 3])
-        self.instances = [*primitives, *items, bytes_, image_, instance_obj, instanse_datdeep]
-
-        # Deep Instances
-        self.deep_instances = [instance_obj, instanse_datdeep]
-
-        # Classes
-        self.classes = [ObjClass, DatClass, DatClassDeep, int]
-
-        # Deep recursion items
-        dict_deep = {1: "a", 2: "b", 7: {11: "2a", 22: "2b", 14: {111: "3a", 222: "3b"}}}
-        list_deep = [1, 2, [11, 22, [111, 222, 333]]]
-        tuple_deep = (1, 2, (11, 22, (111, 222, 333)))
-        set_deep = {1, 2, frozenset({11, 22, frozenset({111, 222, 333})})}  # We use inner frozenset to make it hashable
-        self.deep_items = [list_deep, tuple_deep, set_deep, dict_deep]
-
-        # Oversized Instances
-        str_over = ' '.join(str(i) for i in range(1080))
-        bytes_over = str_over.encode('utf-8')
-        bytearray_over = bytearray(str_over, 'utf-8')
-        memoryview_over = memoryview(bytes_over)
-        list_over = [i for i in range(1080)]
-        tuple_over = tuple(i for i in range(1080))
-        set_over = {i for i in range(1080)}
-        dict_over = {i: i ** 2 for i in range(1080)}
-
-        self.oversized_objects = [str_over, bytes_over, bytearray_over, memoryview_over,
-                                  list_over, tuple_over, set_over, dict_over]
 
 
 # Tests ----------------------------------------------------------------------------------------------------------------
@@ -847,16 +717,16 @@ class TestObjectInfo:
 
     def test_as_str_image_formatting(self, monkeypatch):
         # Create a simple image-like stub
-        class FakeImage:
+        class FakeImageSmall:
             def __init__(self, w, h):
                 self.size = (w, h)
 
-        img = FakeImage(640, 480)
+        img = FakeImageSmall(640, 480)
 
         # Patch acts_like_image in the same module where ObjectInfo is defined
         import sys
         mod = sys.modules[ObjectInfo.__module__]
-        monkeypatch.setattr(mod, "acts_like_image", lambda o: isinstance(o, FakeImage), raising=True)
+        monkeypatch.setattr(mod, "acts_like_image", lambda o: isinstance(o, FakeImageSmall), raising=True)
 
         oi = ObjectInfo.from_object(img)
         assert oi.unit == ("width", "height", "Mpx")
